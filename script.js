d3cca6451726aee71994d21d290775704af55eba
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-06-21 10:54:42 +0200

    :sparkles: added closeModalKey

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const modal = document.querySelector("#modal");
+const closeModalKey = "Escape" // full list of keys: http://gcctech.org/csc/javascript/javascript_keycodes.htm
 
 function openModal() {
     // Active -> will show modal
     modal.classList.add("active");
 }
 
-
 function closeModal() {
     modal.classList.remove("active");
 }
+
+
+// OR on key press ESC
+document.addEventListener("keydown", (e) => {
+    if (e.key === closeModalKey) {
+        modal.classList.remove("active");
+    }
+})
